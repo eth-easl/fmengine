28cedb53262484054254c080a32331b0f7f3e4e7
Xiaozhe Yao <askxzyao@gmail.com>
2023-09-05 13:01:45 +0200

    minor

---
+++ fmengine/utils/token_count.py
@@ -0,0 +1,14 @@
+import os
+from multiprocessing import Pool
+from transformers import AutoTokenizer
+
+def count(tokenizer, text):
+    return len(tokenizer.encode(text, add_special_tokens=False))
+
+def parallel_count(tokenizer_name, texts):
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+    # get cpu count
+    n_processor = os.cpu_count()
+    with Pool(n_processor) as pool:
+        return sum(pool.map(count, [tokenizer] * len(texts), texts))
+
