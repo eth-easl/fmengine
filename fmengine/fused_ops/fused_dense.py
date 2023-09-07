45471e93e14648ba72a5bc257ad2f09cae8d0e74
Xiaozhe Yao <askxzyao@gmail.com>
2023-09-07 17:14:24 +0200

    fused ops

--- fmengine/fused_ops/fused_dense.py
+++ fmengine/fused_ops/fused_dense.py
@@ -8,10 +8,10 @@ from typing import Optional
 import fused_dense_lib as fused_dense_cuda
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import Tensor
-from torch.cuda.amp import custom_bwd, custom_fwd
+import torch.nn.functional as F
 from torch.distributed import ProcessGroup
+from torch.cuda.amp import custom_bwd, custom_fwd
 
 from flash_attn.ops.activations import gelu_bwd, relu_bwd, sqrelu_bwd, sqrelu_fwd
 from flash_attn.utils.distributed import (
--- fmengine/optimizers/loss_func.py
+++ fmengine/optimizers/loss_func.py
@@ -1,10 +1,11 @@
 import torch.nn.functional as F
+from fmengine.fused_ops.fused_crossentropy import SoftmaxCrossEntropyLossFn
 
 def cross_entropy_fn(outputs, labels):
     # unpack
     logits, = outputs
     # all labels are `ignore_index` will cause nan
-    return F.cross_entropy(
+    return SoftmaxCrossEntropyLossFn(
         logits.view(-1, logits.shape[-1]),
         labels.view(-1),
     )
