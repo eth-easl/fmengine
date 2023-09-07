66b27a178ea602d28d9e189f9c2e947a4f728fee
Xiaozhe Yao <askxzyao@gmail.com>
2023-09-08 01:53:54 +0200

    GQA support

--- fmengine/modeling/llama/flash_attention.py
+++ fmengine/modeling/llama/flash_attention.py
@@ -32,6 +32,17 @@ def smart_tokenizer_and_embedding_resize(
         input_embeddings[-num_new_tokens:] = input_embeddings_avg
         output_embeddings[-num_new_tokens:] = output_embeddings_avg
 
+def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
+    """
+    This is the equivalent of torch.repeat_interleave(x, dim=1, repeats=n_rep). The hidden states go from (batch,
+    num_key_value_heads, seqlen, head_dim) to (batch, num_attention_heads, seqlen, head_dim)
+    """
+    batch, num_key_value_heads, slen, head_dim = hidden_states.shape
+    if n_rep == 1:
+        return hidden_states
+    hidden_states = hidden_states[:, :, None, :, :].expand(batch, num_key_value_heads, n_rep, slen, head_dim)
+    return hidden_states.reshape(batch, num_key_value_heads * n_rep, slen, head_dim)
+
 def llama_flash_attn_forward(
     self,
     hidden_states: torch.Tensor,
@@ -49,16 +60,27 @@ def llama_flash_attn_forward(
     bsz, q_len, _ = hidden_states.size()
 
     query_states = self.q_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-    key_states = self.k_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-    value_states = self.v_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+    key_states = self.k_proj(hidden_states).view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+    value_states = self.v_proj(hidden_states).view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
     # [bsz, q_len, nh, hd]
     # [bsz, nh, q_len, hd]
 
     kv_seq_len = key_states.shape[-2]
-    assert past_key_value is None, "past_key_value is not supported"
-
+    if past_key_value is not None:
+        kv_seq_len += past_key_value[0].shape[-2]
     cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
     query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+
+    if past_key_value is not None:
+        # reuse k, v, self_attention
+        key_states = torch.cat([past_key_value[0], key_states], dim=2)
+        value_states = torch.cat([past_key_value[1], value_states], dim=2)
+
+    past_key_value = (key_states, value_states) if use_cache else None
+
+    key_states = repeat_kv(key_states, self.num_key_value_groups)
+    value_states = repeat_kv(value_states, self.num_key_value_groups)
+
     # [bsz, nh, t, hd]
     assert not output_attentions, "output_attentions is not supported"
     assert not use_cache, "use_cache is not supported"
--- scripts/finetune/finetune_llama_1b_local.sh
+++ scripts/finetune/finetune_llama_1b_local.sh
@@ -1,13 +1,13 @@
-deepspeed --num_gpus 1 --num_nodes 1 cli/train.py \
+deepspeed --num_gpus 2 --num_nodes 1 cli/train.py \
     --output_dir .cache/models \
-    --init_ckpt .cache/ckpts/llama-2-1b \
+    --init_ckpt /mnt/scratch/xiayao/cache/pretrained_weights/tinyllama-2-1b \
     --data_path .cache/data/prompt.jsonl \
     --max_seq_len 128 \
     --train_steps 1000 \
     --eval_steps 10 \
     --save_steps 100 \
     --log_steps 1 \
-    --pipe_parallel_size 1 \
+    --pipe_parallel_size 2 \
     --model_parallel_size 1 \
-    --use_flash_attn false \
+    --use_flash_attn true \
     --deepspeed_config ./configs/llama.json
