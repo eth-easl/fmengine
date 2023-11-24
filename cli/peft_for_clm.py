44169eb4f200a984ab495b2b3dc84365131dcdfb
Xiaozhe Yao <askxzyao@gmail.com>
2023-11-24 11:33:43 +0100

    initial peft cli

--- cli/peft_for_clm.py
+++ cli/peft_for_clm.py
@@ -1,26 +1,19 @@
-import torch
 import argparse
 import transformers
-
-from tqdm import tqdm
 from accelerate import Accelerator
-from torch.utils.data import DataLoader
-from peft.utils.other import fsdp_auto_wrap_policy
-from peft import get_peft_config, get_peft_model, LoraConfig, TaskType
-
+from peft import get_peft_model, LoraConfig, TaskType
 from fmengine.dataloader.jsonl_loader import get_jsonl_dataset
-from fmengine.dataloader.jsonl_loader import AutoregressiveLanguageModelDataCollator
 
 def train(args):
     import os
     os.environ["WANDB_PROJECT"] = args.project_name
-    accelerator = Accelerator()
     peft_config = LoraConfig(
         task_type=TaskType.CAUSAL_LM,
         inference_mode=False,
-        r=args.lora_rank,
-        lora_alpha=args.lora_alpha,
-        lora_dropout=args.lora_dropout,
+        # just use default parameters
+        # r=args.lora_rank,
+        # lora_alpha=args.lora_alpha,
+        # lora_dropout=args.lora_dropout,
     )
     model = transformers.AutoModelForCausalLM.from_pretrained(args.model_name_or_path)
     
@@ -34,7 +27,9 @@ def train(args):
     """
     handle special tokens
     """
-    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.pad_token_id = tokenizer.bos_token_id
+    tokenizer.pad_token = tokenizer.bos_token
+
     tokenizer.padding_side = "left"
     model.print_trainable_parameters()
 
@@ -64,14 +59,14 @@ def train(args):
         ),
         data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
     )
-    model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+    model.config.use_cache = False
     trainer.train()
     trainer.save_model(args.output_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-name-or-path", type=str, required=True)
-    parser.add_argument("--lora-rank", type=int, required=True)
+    parser.add_argument("--lora-rank", type=int, default=8)
     parser.add_argument("--lora-alpha", type=int, default=32)
     parser.add_argument("--lora-dropout", type=float, default=0.1)
     parser.add_argument("--data-path", type=str, required=True)
--- scripts/peft/run_peft_clm.sh
+++ scripts/peft/run_peft_clm.sh
@@ -8,4 +8,4 @@ singularity run --nv \
 --bind $PWD:/workspace \
 --pwd /workspace \
 fmsys.sif \
-python cli/peft_for_clm.py --model-name-or-path openlm-research/open_llama_3b_v2 --lora-rank 32 --data-path .cache/data/sqlg.train.ar.jsonl --wandb-run-name openllama-3b-sqlg-r8 --project-name ft-research --max-seq-len 512
+CUDA_VISIBLE_DEVICES=0 python cli/peft_for_clm.py --model-name-or-path openlm-research/open_llama_3b_v2  --data-path /datasets/data.jsonl --wandb-run-name openllama-3b-chat-r8 --project-name ft-research --max-seq-len 512
