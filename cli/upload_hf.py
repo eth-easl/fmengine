8ed2fe8f2bfceb344acb0324ff03720e93f39d9b
Xiaozhe Yao <askxzyao@gmail.com>
2023-12-12 10:34:35 +0100

    wip: gated linear attention

--- cli/upload_hf.py
+++ cli/upload_hf.py
@@ -16,12 +16,12 @@ def upload(args):
     )
     for step in steps:
         print(f"Exporting {step}")
-        to_hf_model(
-            in_model_path=args.ckpt_path,
-            model_family=args.base_model,
-            out_model_path=os.path.join(args.temp_path, step),
-            step=step,
-        )
+        # to_hf_model(
+        #     in_model_path=args.ckpt_path,
+        #     model_family=args.base_model,
+        #     out_model_path=os.path.join(args.temp_path, step),
+        #     step=step,
+        # )
         shutil.copytree(
             os.path.join(args.temp_path, step),
             os.path.join(args.temp_path, "hf"),
@@ -34,7 +34,7 @@ def upload(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ckpt-path", type=str, required=True)
+    parser.add_argument("--file-path", type=str, required=True)
     parser.add_argument("--base-model", type=str, required=True)
     parser.add_argument("--temp-path", type=str, required=True, default=".cache/temp")
     parser.add_argument("--hf-org", type=str, required=True)
